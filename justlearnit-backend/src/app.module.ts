25b6f061050621a61b480d60ce7a56df7defed6a
RCzerwiec <czerwiecr1999@gmail.com>
2023-01-26 16:46:00 +0100

    Implemented posts controller

--- justlearnit-backend/src/app.module.ts
+++ justlearnit-backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsService } from './posts/posts.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ justlearnit-backend/src/posts/posts.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsController } from './posts.controller';
+import { Post, PostSchema } from './post.schema';
+import { PostsService } from './posts.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+  ],
+  controllers: [PostsController],
+  providers: [PostsService],
+})
+export class PostsModule {}
