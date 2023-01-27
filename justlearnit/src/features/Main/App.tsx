a87b9a470df5967844794de5a33484c7c478705e
RCzerwiec <czerwiecr1999@gmail.com>
2023-01-27 15:56:04 +0100

    env  added to justlearnit front

--- justlearnit/src/features/Main/App.tsx
+++ justlearnit/src/features/Main/App.tsx
@@ -44,7 +44,7 @@ function App() {
   });
 
   const fetchBooks = useCallback(async () => {
-    const res = await axios.get("http://localhost:5000/posts");
+    const res = await axios.get(process.env.REACT_APP_DATABASE!);
     setData(res.data);
   }, []);
 
