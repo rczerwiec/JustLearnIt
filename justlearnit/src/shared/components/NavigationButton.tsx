29b2a35e73afb95f8b326dd5dc0eebbe1414aaf2
RCzerwiec <czerwiecr1999@gmail.com>
2023-01-27 15:41:17 +0100

    motion framer navbutton animation added

--- justlearnit/src/shared/components/NavigationButton.tsx
+++ justlearnit/src/shared/components/NavigationButton.tsx
@@ -1,18 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
-interface PropsTypes{
-    icon: string,
-    className: string,
-    navigateTo: string,
+interface PropsTypes {
+  icon: string;
+  className: string;
+  navigateTo: string;
 }
 
-function NavigationButton({icon, className, navigateTo}: PropsTypes){
-    
-    return(
-        <Link to={`/${navigateTo}`}>
-        <div className={className}><img className="m-3" src={icon} alt="Add Icon"></img></div>
-        </Link>
-    )
+function NavigationButton({ icon, className, navigateTo }: PropsTypes) {
+  return (
+    <Link to={`/${navigateTo}`}>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className={className}
+      >
+        <img className="m-3" src={icon} alt="Add Icon"></img>
+      </motion.button>
+    </Link>
+  );
 }
 
 export default NavigationButton;
