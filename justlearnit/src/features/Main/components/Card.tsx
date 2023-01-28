c9db41f94a56f5eb2da82bb4e2f4a15e91f5f9e3
czerwiecr <czerwiecr1999@gmail.com>
2023-01-28 22:18:38 +0100

    fix remove button

--- justlearnit/src/features/Main/components/Card.tsx
+++ justlearnit/src/features/Main/components/Card.tsx
@@ -14,48 +14,58 @@ interface ICard {
 }
 
 function Card({ note, onClick, onRemove }: ICard) {
-
-  const renderedDescription = note.description.map((m: string) => {
-    if (m.length <= 100) {
-      return (
-        <div
-          key="0"
-          className="text-center text-sm m-4"
-          dangerouslySetInnerHTML={{ __html: m }}
-        ></div>
-      );
-    } else {
-      let newM = m.slice(0, 50);
-      newM = newM + "<div>Read more...</div>";
-      return (
-        <div
-          key="0"
-          className="text-center text-sm m-4"
-          dangerouslySetInnerHTML={{ __html: newM }}
-        ></div>
-      );
-    }
-  });
+  let renderedDescription;
+  if(note.description.length>0){
+    renderedDescription = note.description.map((m: string) => {
+      if (m.length <= 50) {
+        return (
+          <div
+            key="0"
+            className="text-center text-sm m-4"
+            dangerouslySetInnerHTML={{ __html: m }}
+          ></div>
+        );
+      } else {
+        let newM = m.slice(0, 50);
+        newM = newM + "<div>Read more...</div>";
+        return (
+          <div
+            key="0"
+            className="text-center text-sm m-4"
+            dangerouslySetInnerHTML={{ __html: newM }}
+          ></div>
+        );
+      }
+    });
+  }
+  else{
+    renderedDescription= <div
+    key="0"
+    className="text-center text-sm m-4"
+  ></div>
+  }
+  
 
   return (
-    <div
-      className="bg-grayMain m-4 rounded-md hover:cursor-pointer shadow-md"
-      onClick={() => {
-        onClick(note);
-      }}
-    >
-      <div className="bg-graySecondary m-4 p-3 text-center text-3xl">
-        {note.tag}
-      </div>
-      <div className="text-center text-xl">{note.title}</div>
-      {renderedDescription}
-      <div className="flex justify-end">
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="" onClick={() => { onRemove(note._id) }}><img className="m-3 w-5" src={TrashIcon}></img></motion.button>
-      </div>
+    <div className="flex flex-col justify-between h-80 min-h-fit bg-grayMain m-4 rounded-md hover:cursor-pointer shadow-md">
+      <div
+        onClick={() => {
+          onClick(note);
+        }}
+      >
+        <div className="bg-graySecondary m-4 p-3 text-center text-3xl">
+          {note.tag}
+        </div>
+        <div className="text-center text-xl">{note.title}</div>
+        {renderedDescription}
 
+      </div>
+      <div className="flex justify-end">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="" onClick={() => { onRemove(note._id) }}><img className="m-3 w-5" src={TrashIcon}></img></motion.button>
+      </div>
     </div>
   );
 }
